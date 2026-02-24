03863043f81a8cdfaab0c7e6d7cf2fc2a7ac9312
ayeshaaaslam <ayeshaaslam3698@gmail.com>
2026-02-24 21:16:10 +0500

    Add datatypes example

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as modern
+
+//alert(3+3); we are using node js not browser, so alert is not available
+let name = "Ayesha Aslam";
+let age = 18;
+let isStudent = true;
+
+//number => 2 to the power of 53
+//bigint => larger than 2 to the power of 53
+//string => sequence of characters " "
+//boolean => true or false
+//undefined => variable declared but not assigned a value
+//null => explicitly set to no value
+// symbol => unique identifier
+
+//object => collection of key-value pairs
+
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof null); // typeof null returns "object" due to a historical bug in JavaScript, but it is actually a primitive value representing the absence of any object value.
